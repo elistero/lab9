4c0b96bdd17a64f7d7bdbed85eb04712e933b408
elsa krauskeite <76253481+elistero@users.noreply.github.com>
2020-12-26 00:23:03 -0500

    3.0

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+namespace Laba3;
+
+use Krauskeite\MyLog;
+
+require_once "core\EquationInterface.php";
+require_once "core\LogAbstract.php";
+require_once "core\LogInterface.php";
+require_once "Krauskeite\Line.php";
+require_once "Krauskeite\MyLog.php";
+require_once "Krauskeite\Quadratic.php";
+MyLog::log("elsa");
+MyLog::write();
