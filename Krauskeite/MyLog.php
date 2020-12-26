25ef6154e51809e52821a9fff0a8acf01696aa80
elsa krauskeite <76253481+elistero@users.noreply.github.com>
2020-12-26 00:43:54 -0500

    5.0

---
+++ Krauskeite/MyLog.php
@@ -0,0 +1,36 @@
+<?php namespace Krauskeite;
+
+use core\LogAbstract;
+use core\LogInterface;
+
+
+Class MyLog extends LogAbstract implements LogInterface {
+
+    public function _log(String $str){
+        $this->log[]=$str;
+    }
+
+    /**
+     * @param String $str строка для записи в массив лога
+     */
+    public static function log(String $str){
+        self::Instance()->_log($str);
+	}
+    
+    public function _write(){
+        
+        foreach($this->log as $value){
+            echo $value."\n";
+			
+            file_put_contents(__DIR__ . "\..\log\\$b.log", trim($value."\r\n") . PHP_EOL, FILE_APPEND);
+        }
+        
+    }
+    
+    public static function write(){
+        self::Instance()->_write();
+    }
+
+}
+
+?>
